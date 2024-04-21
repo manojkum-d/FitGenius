2867cac0ae1ff5e751ce8ace394c15d84aa92dc4
manojkum-d <141437046+manojkum-d@users.noreply.github.com>
2024-04-21 16:05:58 +0530

    Update PythonML.jsx

--- src/components/pythonwala/PythonML.jsx
+++ src/components/pythonwala/PythonML.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PythonML = () => {
   return (
     <iframe
-        src="http://localhost:8501/"
+        src="https://fitgenius-ai-1.onrender.com"
         title="Streamlit App"
         // style={{ width: '100%', height: '600px', border: 'none' }}
         className='w-[100%] h-[85%]'
